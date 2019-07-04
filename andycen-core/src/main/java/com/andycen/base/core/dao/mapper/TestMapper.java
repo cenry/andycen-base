3b62455d0ecc10ec1e80dc7d12ff430c15515d62
cenry <cenruyi123@gmail.com>
2019-07-04 16:24:45 +0800

    update

---
+++ andycen-core/src/main/java/com/andycen/base/core/dao/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package com.andycen.base.core.dao.mapper;
+
+import com.andycen.base.core.meta.po.TestPo;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author cenruyi
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface TestMapper extends Mapper<TestPo> {
+
+}
